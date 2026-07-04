d3f7140309300c7f2c63918094b5618551f0769e
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-07-04 14:59:58 +0530

    First Commit

---
+++ src/main/java/com/mclods/oa2_client/Oa2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.mclods.oa2_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oa2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oa2ClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mclods/oa2_client/Oa2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mclods.oa2_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oa2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
