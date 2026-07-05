cb22d5becc94124ebb8892c6a7913dbbb146eb0f
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-07-05 14:19:10 +0530

    Added OAuth2 Client security filter chain Added test controller endpoints

---
+++ src/main/java/com/mclods/oa2_client/configurations/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.mclods.oa2_client.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
